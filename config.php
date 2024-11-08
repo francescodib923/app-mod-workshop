64faeaea3d3d31a490884d8c53d078ff648906ff
francescodib923 <francesco.dib923@gmail.com>
2024-11-08 12:03:30 +0000

    modifica config e dockerfile per usare env

--- config.php
+++ config.php
@@ -1,9 +1,13 @@
 <?php
 // Configurazione del database
-$db_host = 'localhost';
-$db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+$db_host = getenv('DB_HOST') ?: '34.154.208.192';
+$db_name = getenv('DB_NAME') ?: 'image_catalog';
+$db_user = getenv('DB_USER') ?: 'appmod-phpapp-user';
+$db_pass = getenv('DB_PASS') ?: 'PippoBardo92!';
+// $db_host = getenv()'34.154.208.192';
+// $db_name = 'image_catalog';
+// $db_user = 'appmod-phpapp-user';
+// $db_pass = 'PippoBardo92!';
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
